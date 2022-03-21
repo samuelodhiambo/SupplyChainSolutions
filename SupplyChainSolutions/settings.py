2386232a0aeea49730db704d3b7139dcfd4ecfc7
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-03-22 02:54:57 +0300

    It will manage static files and template inheritance

--- SupplyChainSolutions/settings.py
+++ SupplyChainSolutions/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'SupplyChainSolutions.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# MEDIA Files
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
