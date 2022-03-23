81774683b3a3e816e094b16499e7cdda60d7acc4
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-03-24 02:04:53 +0300

    patch: It will link home page to product database

--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from product.models import Product
 
 # Create your views here.
 def homeView(request, template="home.html"):
-    context = {}
+    products = Product.objects.all()
+    context = {
+        'products': products,
+    }
     return render(request, template, context)
