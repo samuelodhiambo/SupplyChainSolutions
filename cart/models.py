1345e15a52f68fef886ea35e78d0bc277cf8b5a9
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-03-23 22:12:54 +0300

    patch: It will add a responsive cart

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from product.models import Product
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Processing', 'Processing'),
+        ('Delivered', 'Delivered'),
+    )
+    order_amount = models.IntegerField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    date_ordered = models.DateTimeField(auto_now_add=True, null=True)
+    time_ordered = models.TimeField(auto_now_add=True, null=True, blank=True)
+    status = models.CharField(choices=STATUS, max_length=200, null=True, default='Pending')
+
+    class Meta:
+        db_table = 'Order'
+
+    def __str__(self) -> str:
+        return self.product.product_name
--- cart/static/js/cart.js
+++ cart/static/js/cart.js
@@ -3,6 +3,43 @@ $(document).ready(function(){
         $(this).children("td").children(".plus").click(function(){
             let newVal = parseInt($(this).siblings("input").val()) + 1
             $(this).siblings("input").val(newVal)
+
+            var serializedData = {
+                'csrfmiddlewaretoken': $("[name='csrfmiddlewaretoken']").val(),
+                'order_amount': parseInt($(this).siblings("input").val())
+            }
+            // make POST ajax call
+            $.ajax({
+                type: 'POST',
+                url: "/cart/ajaxAdd/",
+                data: serializedData,
+                success: function (response) {
+                    // on successfull creating object
+                    // 1. clear the form.
+                    $("#friend-form").trigger('reset');
+                    // 2. focus to nickname input 
+                    $("#id_nick_name").focus();
+
+                    // display the newly friend to table.
+                    var instance = JSON.parse(response["instance"]);
+                    var fields = instance[0]["fields"];
+                    $("#my_friends tbody").prepend(
+                        `<tr>
+                        <td>${fields["nick_name"]||""}</td>
+                        <td>${fields["first_name"]||""}</td>
+                        <td>${fields["last_name"]||""}</td>
+                        <td>${fields["likes"]||""}</td>
+                        <td>${fields["dob"]||""}</td>
+                        <td>${fields["lives_in"]||""}</td>
+                        </tr>`
+                    )
+                },
+                error: function (response) {
+                    // alert the error if any error occured
+                    alert(response["responseJSON"]["error"]);
+                }
+            })
+
         })
 
         $(this).children("td").children(".minus").click(function(){
